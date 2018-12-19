c35d92ae78eba423a1cd2abb68c5d1fc2cb6973f
rzfreitas <zoid_freitas@yahoo.com.br>
2018-12-19 11:48:14 -0200

    commit 19/12

--- template-parts/content-conversion-row.php
+++ template-parts/content-conversion-row.php
@@ -18,15 +18,7 @@
 	<?php elseif ( get_field('modulo') == 'doc' ): ?>
 	<div class="bg-doc conversion-row">
 	<?php elseif ( get_field('modulo') == 'liracorp' ): ?>
-	<div class="bg-liracorp">
-	<?php endif ?>
-
-	<?php if( get_field('modulo') == 'lira' ): ?>
-	<div class="bg-lira conversion-row">
-	<?php elseif ( get_field('modulo') == 'auditoria' ): ?>
-	<div class="bg-aud conversion-row">
-	<?php elseif ( get_field('modulo') == 'gaia' ): ?>
-	<div class="bg-gaia conversion-row">
+	<div class="bg-liracorp conversion-row">
 	<?php endif ?>
 		<div class="container">
 			<div class="row d-flex text-center">
