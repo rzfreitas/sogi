bd3a01dd36b5a2bbd3de85684c7e1fc54b1da5cb
Rodolpho Freitas <34680178+rzfreitas@users.noreply.github.com>
2019-01-07 02:57:43 -0200

    outs e cursos

---
+++ template-parts/template-cursos.php
@@ -0,0 +1,143 @@
+<?php /* Template Name: Cursos Outsourcing */ 
+
+get_header(); ?>
+<section>
+<div class="bg-yellow">
+	<div class="container p-t-100">
+		<div class="row dp-flex">
+			<div class="col-md-6">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/cursos-outsourcing.png" alt="cursos outsourcing">
+			</div>
+			<div class="col-md-6">
+				<div class="p-t-70 text-center">
+					<h1>Curso EAD LEAD SOGI</h1>
+					<p>O SOGI é considerado o mais completo Sistema Online focado em Gestão Integrada disponível no mercado. Através deste curso, os profissionais poderão conhecer a fundo todos os módulos da plataforma desenvolvida pela Verde Ghaia, sabendo exatamente como utilizá-la.</p>
+					<div class="p-t-50">
+						<a href="" class="btn-def btn-roxo">INSCREVA-SE AGORA!</a>
+					</div>					
+				</div>
+				<div class="p-t-70">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/info-cursos.png" alt="informações cursos outsourcing">
+				</div>	
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+<section>
+	<div class="bg-marinho">
+		<div class="container p-t-100 p-b-100">
+			<div class="row dp-flex">
+				<div class="col-md-6 box-cian">
+					<p>Entenda como é importante para você conhecer o SOGI. Uma ferramenta totalmente pensada para simplificar o monitoramento da legislação aplicável e da gestão integrada dos processos nas empresas.</p>
+				</div>
+				<div class="col-md-6">
+					VIDEO*
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+<div class="bg-cinza">
+	<div class="container p-t-100 p-b-100">
+		<h2>PÚBLICO ALVO</h2>
+		<p class="p-t-25 p-b-100">Colaboradores de organizações que utilizam o SOGI. Profissionais e estudantes em geral que querem aprimorar seus conhecimentos ou que pretendem ser parceiros da Verde Ghaia ou que apostam em um diferencial para entrar em grandes empresas que possuem essa ferramenta de gestão integrada.</p>
+		<ul class="dp-flex list-cursos"> 
+			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/carga-horaria.png" alt="carga horária outsourcing">
+			CARGA HORÁRIA: 12 HORAS / AULAS</li>
+			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/certificado.png" alt="certificado outsourcing">CERTIFICADO GERADO AUTOMATICAMENTE, MEDIANTE CONCLUSÃO E APROVAÇÃO DO CURSO</li>
+			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/tutores.png" alt="tutores outsourcing">TUTORES DISPONÍVEIS DURANTE TODO O CURSO PARA ACOMPANHAR E TIRAR AS DÚVIDAS DOS ALUNOS</li>
+			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/videos.png" alt="videos outsourcing">VÍDEOS DE APRESENTAÇÃO E TUTORIAIS EXPLICATIVOS DO SISTEMA</li>
+			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/apostilas.png" alt="apostilas outsourcing">APOSTILAS E SLIDES TEÓRICOS</li>
+			<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/avaliacoes.png" alt="avaliacoes outsourcing">AVALIAÇÕES INTERATIVAS EM CADA MÓDULO</li>
+		</ul>
+	</div>
+</div>
+</section>
+
+<section>
+	<div class="bg-cian">
+		<div class="container p-t-100 p-b-100">
+			<div class="row dp-flex">
+				<div class="col-md-5">
+					<h2 class="v-center">POR QUE REALIZAR O CURSO?</h2>
+				</div>
+				<div class="col-md-7">
+					<ul class="list-cursos2">
+						<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/conhecimento.png" alt="conhecimento outsourcing">Adquirir conhecimento completo, teórico e prático, sobre a utilização do Sistema SOGI.</li>
+						<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/software.png" alt="software outsourcing">Ter pleno domínio sobre esse software que é essencial para a realização de uma gestão integrada nas organizações.</li>
+						<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/sgi.png" alt="sgi outsourcing">Conquistar um grande diferencial na área de sistema de gestão e destacar-se no mercado.</li>
+						<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/gestao.png" alt="gestao outsourcing">Ampliar a formação na área de gestão de riscos, compliance e prevenção, levando mais estratégias de sustentabilidade para as organizações.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container p-t-100 p-b-100">
+		<div class="row">
+			<div class="col-md-6">
+				<h2>CONFIRA O CONTEÚDO PROGRAMÁTICO</h2>
+			</div>
+			<div class="col-md-6">
+				<p>No curso EAD LEAD SOGI, o sistema é apresentado de forma didática e interativa, utilizando diferentes  materiais e recursos de mídias para facilitar a compreensão do aluno e usuário do software.</p>
+			</div>
+		</div>
+		<div class="p-t-70 text-center">
+			<a href="" class="btn-def btn-roxo">VEJA O CONTEÚDO</a>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="bg-img-blue p-t-100 p-b-100">
+		<div class="container">
+			<h2>CURSO ON LINE</h2>
+			<p class="p-t-50">Através do curso LEAD SOGI o aluno conhecerá todos os módulos do sistema, percorrendo cada uma de suas ferramentas e funções. Tornando-se apto a implantar e utilizar o software nas organizações. O SOGI foi criado para realizar o monitoramento dos requisitos legais aplicáveis às organizações, além dos aspectos e impactos ambientais e perigos e riscos da atividade. Através dele, é possível também realizar auditorias internas e de conformidade legal, gerenciar não conformidades, licenças e fornecedores. E o mais importante, realizar a gestão da Grovernança, Riscos e Compliance da sua organização.</p>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container p-t-100 p-b-100">
+		<div class="row">
+			<div class="col-md-5">
+				<h2>APRENDA COM OS MELHORES</h2>
+				<p>Os materiais didáticos do curso EAD SOGI são elaborados pela equipe de Suporte da Verde Ghaia, especialistas que lidam diretamente com o sistema em seu dia-a-dia no atendimento ao cliente. Além disso, os professores atuam como tutores durante todo o curso, acompanhando os alunos durante todo o processo de aprendizagem.</p>
+			</div>
+			<div class="col-md-3 text-center">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/cursos-outsourcing/foto.png" alt="">
+			</div>
+			<div class="col-md-4">
+				<h5 class="p-t-50">Viviane Mara Diniz</h5>
+				<p>Gestora de Projetos, Coordenadora do Departamento de Suporte ao cliente e responsável pelo conteúdo programático do curso SOGI.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="bg-cursos">
+		<div class="container">
+			<div class="row dp-flex">
+				<div class="col-md-6 bg-opacity">
+					<h2>APRENDA DE <br> FORMA DINÂMICA <br> E INTERATIVA</h2>
+					<p class="p-t-50 p-b-50">Os materiais, que vão de apostilas teóricas a videoaulas e tutorias, apresentam o sistema de forma dinâmica e interativa. Assim, o usuário terá facilidade na hora de utilizar a plataforma e saberá tudo o que precisa para tirar o melhor proveito de suas inúmeras funcionalidades. </p>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>lira"><h4>QUER APRENDER APENAS O MÓDULO LIRA? CLIQUE AQUI.</h4></a>
+				</div>
+				<div class="col-md-6 text-center">
+					<div class="v-center">
+						<a href="" class="btn-def btn-roxo">INSCREVA-SE AGORA <br> <small>Investimento R$ 260,00</small></a>
+					</div>					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
--- template-parts/template-outsourcing.php
+++ template-parts/template-outsourcing.php
@@ -2,13 +2,74 @@
 
 get_header(); ?>
 
-<?php get_template_part('template-parts/content' , 'tabs'); ?>
+<section>
+	<div class="bg-out">
+		<div class="container v-center">
+			<div class="row">
+				<div class="col-md-7">
+				
+				</div>
+				<div class="col-md-5">					
+					<h1>Está sem tempo? <br> Converse com a gente!</h1>
+					<p>Mantenha seu módulo Lira sempre atualizado. Requisitos legais, obrigações, evidências e muito mais, tudo do jeitinho que tem que ser.</p>									
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 <section>
-	<?php $image = get_field('img_outsrc');
-	if( !empty($image) ): ?>
-		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-	<?php endif; ?>
+	<div class="container p-t-100 p-b-100">
+		<div class="row">
+			<div class="col-md-6">
+				<p><strong>O serviço de Outsourcing LIRA veio para sanar o problema de diversos clientes que, por diferentes motivos, não conseguem deixar seu módulo do SOGI atualizado.Agora é possível contratar serviço jurídico para realização de todo o monitoramento dos seus requisitos legais, como:</strong></p>
+				<div class="pointer p-t-50 text-center">
+					<img class="p-b-15" src="<?php echo get_template_directory_uri(); ?>/assets/imagens/download-arrow.png" alt="baixar apresentação"><br>
+					<span>BAIXE A APRESENTAÇÃO</span>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<ul class="list-disc">
+					<li>Evidenciação de obrigações,</li>
+					<li>Validação de requisitos,</li>
+					<li>Avaliação da aplicabilidade das obrigações,</li>
+					<li>Incorporação de áreas de abrangência e </li>
+					<li>Criação plano de ações.</li>
+					<div class="p-t-50 text-center">
+						<a href="" class="btn-def btn-roxo">Contratar</a>
+					</div>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="bg-contratar p-t-100">
+		<div class="container">
+			<h2>Por que <br> contratar?</h2>
+		</div>
+		<div class="container p-t-50 p-b-100">
+			<ul class="out dp-flex flex-wrap">
+				<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/home/analytics.png" alt="monitoramento">
+				Atualização mensal dos requisitos, obrigações e evidências do Módulo Lira.</li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/home/relatorios.png" alt="relatorios">Excelente para empresas que fazem Auditoria 100%, pois ações são diluídas ao longo do ano.</li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/home/indicadores.png" alt="indicadores">Aumenta as chances de estar entre os melhores no Prêmio Compliance Brasil.</li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/home/graficos.png" alt="monitoramento">Evita estresse em períodos de pré-auditoria de certificação, pois tudo estará em dia.</li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/home/seguranca.png" alt="segurança">Utilização de mão de obra especializada e experiente no monitoramento legal.</li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/home/filtros.png" alt="Filtros">Maior apoio técnico e suporte dos responsáveis pela metodologia SOGI.</li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container p-t-100 p-b-100">
+		<h2 class="text-center">Como funciona</h2>
+		<div class="p-t-100">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/imagens/outsourcing/como-fuciona-outsourcing.png" alt="como fuciona outsourcing">
+		</div>
+	</div>
 </section>
 
 <?php get_footer(); ?>
